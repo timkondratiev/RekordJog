dab26cedffb4e05eca8878bcbd933213cddd20cf
Timofei Kondratev <t.a.kondratiev@gmail.com>
2024-09-28 21:39:11 +0400

    Delete old files

--- changeMidiDevices.py
+++
@@ -1,5 +0,0 @@
-from functions.set_midi_devices import set_midi_devices
-try:
-    set_midi_devices()
-except KeyboardInterrupt:
-    print("\nExiting, bye.")
--- functions/clear_terminal.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def clear_terminal():
-    # For Windows
-    if os.name == 'nt':
-        os.system('cls')
-    # For Unix/Linux/MacOS
-    else:
-        os.system('clear')
